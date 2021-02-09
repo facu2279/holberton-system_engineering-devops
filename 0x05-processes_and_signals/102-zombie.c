9e5f5d981b31551df627e955d7a6c03e2a3a208d
facu2279 <2279@holbertonschool.com>
2021-02-09 02:56:14 +0000

    Fix task

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -2,36 +2,39 @@
 #include <stdlib.h>
 #include <unistd.h>
 /**
- * infinite_while - holberton function
- * Return: 0
- */
+* infinite_while - holberton function
+* Return: 0
+*/
 int infinite_while(void)
 {
-    while (1)
-    {
-        sleep(1);
-    }
-    return (0);
+while (1)
+{
+sleep(1);
+}
+return (0);
 }
 /**
- * main - main function
- * Return: 0
- */
+* main - main function
+* Return: 0
+*/
 int main(void)
 {
-    pid_t hijo;
-	int i= 1;
+pid_t hijo;
+int i = 1;
 
-	while(i < 6)
-	{
-		hijo = fork();
-        if (hijo > 0)
-        {
-            printf("Zombie process created, PID: %u\n", hijo);
-        }
-        
-        i++;
-	}
-	infinite_while();
-	return (0);
+while (i < 6)
+{
+hijo = fork();
+if (hijo > 0)
+{
+printf("Zombie process created, PID: %d\n", hijo);
+}
+else
+{
+    exit(0);
+}
+i++;
+}
+infinite_while();
+return (0);
 }

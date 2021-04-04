bc0d89d41ff04bd71870a4d3d9f407c5c80ffc28
facu2279 <2279@holbertonschool.com>
2021-04-04 23:53:46 +0000

    Add advanced task

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# Made by Facundo Diaz to Holberton School 2021
+file_line { 'change private key':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/holberton',
+  match  => 'IdentityFile ~/.ssh/id_rsa',
+}
+file_line { 'no password':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  match  => 'PasswordAuthentication yes',
+  line   => 'PasswordAuthentication no',
+}

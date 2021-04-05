de4c001dd5363dcf7596c0c8465f0dc827e5c7f5
facu2279 <2279@holbertonschool.com>
2021-04-05 21:44:32 +0000

    First commit

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Made by Facundo Diaz for Holberton School 2021
+
+file { 'holberton':
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}

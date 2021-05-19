0fc77306ab46c18c242597fa24dfc43aa16ba826
facu2279 <facu2279@holbertonschool.com>
2021-05-19 02:59:04 +0000

    Fix

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,4 +1,4 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 """Write a function that queries the Reddit API and prints the titles of
    the first 10 hot posts listed for a given subreddit"""
 import requests

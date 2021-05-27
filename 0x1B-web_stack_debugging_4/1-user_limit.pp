c0efe3527747e4bf67d31af66a8254958adf2e3c
facu2279 <facu2279@holbertonschool.com>
2021-05-27 21:40:44 +0000

    Fix

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,9 @@
+# Made by TFacundo Diaz for Holberton School 2021
+exec { 'maxsoft' :
+  command => 'sed -i s/4/50000/ /etc/security/limits.conf',
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
+}
+exec { 'maxhard' :
+  command => 'sed -i s/5/50000/ /etc/security/limits.conf',
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
+}
